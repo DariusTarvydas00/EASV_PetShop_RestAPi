f1af5c949b9dea7a61240323a00e6da0bf9e86f7
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-09-21 15:10:14 +0200

    RestApi with Customer
    Working

---
+++ EASV_PetShop_RestAPi/Controllers/CustomerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EASV_PetShop.Core.ApplicationService;
+using EASV_PetShop.Core.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EASV_PetShop_RestAPi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            return _customerService.GetAllCustomers();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Customer> Get(int id)
+        {
+            return _customerService.FindCustomerById(id);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Customer> Put(int id, [FromBody] Customer customer)
+        {
+            if (id < 1 || id != customer.Id)
+            {
+                return BadRequest("Parameter Id and Customer Id must be same");
+            }
+
+            return Ok(_customerService.UpdateCustomer(customer));
+        }
+
+        [HttpPost]
+        public ActionResult<Customer> Post([FromBody] Customer customer)
+        {
+            if (string.IsNullOrEmpty(customer.FirstName))
+            {
+                return BadRequest("First Name is required");
+            }
+
+            return _customerService.CreateCustomer(customer);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Customer> Delete(int id)
+        {
+            var customer = _customerService.DeleteCustomer(id);
+            if (customer == null)
+            {
+                return StatusCode(404, "Did not find customer with" + id);
+            }
+
+            return Ok($"Customer with Id: {id} is Deleted");
+        }
+    }
+}
---
+++ EASV_PetShop_RestAPi/Startup.cs
@@ -0,0 +1,54 @@
+using ClassLibrary1Infrastructure.Repositories;
+using EASV_PetShop.Core.ApplicationService;
+using EASV_PetShop.Core.ApplicationService.Services;
+using EASV_PetShop.Core.DomainService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace EASV_PetShop_RestAPi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "EASV_PetShop_RestAPi", Version = "v1"});
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EASV_PetShop_RestAPi v1"));
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
